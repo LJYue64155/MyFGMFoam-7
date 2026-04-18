b7fc4b8c016cca8d0611b6a02b5fb9c0a8ba3d9b
LJY20010321@outlook.com <LJY20010321@outlook.com>
2026-04-19 07:06:39 +0800

    删除FGM_example文件夹

--- .vscode/of-gdb.sh
+++ .vscode/of-gdb.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-. /OpenFOAM/of7/OpenFOAM-7/etc/bashrc WM_NCOMPROCS=2;
+# 获取传入的完整命令（包括求解器路径和参数）
+# 废了，不能调试！！
+CMD="$@"
 /usr/bin/gdb "$@"
+
+. /home/liujiayue/OpenFOAM/of7/OpenFOAM-7/etc/bashrc WM_NCOMPROCS=2;
+. /home/liujiayue/OpenFOAM/of7/liujiayue-7/applications/MyFGMFoam/src/bashrc;
+
+exec $CMD
